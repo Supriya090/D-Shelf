625e14872f657138b7a035b7dba019374a946c07
prab205 <prabinpaudel43@gmail.com>
2022-01-31 20:09:06 +0545

    pdf viewer done

--- src/components/Write.jsx
+++ src/components/Write.jsx
@@ -1,32 +1,11 @@
-import { makeStyles, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
 import React, { useState } from "react";
-import { EditorState, convertToRaw } from "draft-js";
-import { Editor } from "react-draft-wysiwyg";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import PDFViewer from "./PDFViewer"
+
 
 const useStyles = makeStyles((theme) => ({
   writePageContent: {
     display: "flex",
-  },
-  wrapperClass: {
-    padding: "1rem",
-    border: "1px solid #ccc",
-    margin: "2rem",
-    borderRadius: "5px",
-  },
-  editorClass: {
-    backgroundColor: "#141414",
-    padding: "0px 15px",
-    color: "#fff",
-    border: "1px solid #ccc",
-    fontFamily: "Rubik, sans-serif",
-    height: "350px",
-    borderRadius: "3px",
-  },
-  toolbarClass: {
-    border: "1px solid #ccc",
-    backgroundColor: "#000",
-    fontFamily: "Rubik, sans-serif",
   },
   uploadContent: {
     padding: "1rem",
@@ -34,34 +13,24 @@ const useStyles = makeStyles((theme) => ({
     margin: "2rem",
     borderRadius: "5px",
     color: "#fff",
+    width: "1700px",
   },
 }));
 
 const Write = () => {
   const {
-    wrapperClass,
-    editorClass,
-    toolbarClass,
     writePageContent,
     uploadContent,
   } = useStyles();
 
-  const [editorState, setEditorState] = useState(() =>
-    EditorState.createEmpty()
-  );
-
   return (
     <div className={writePageContent}>
-      <Editor
-        editorState={editorState}
-        onEditorStateChange={setEditorState}
-        wrapperClassName={wrapperClass}
-        editorClassName={editorClass}
-        toolbarClassName={toolbarClass}
-      />
-      <div className={uploadContent}>Upload Content</div>
+      <div className={uploadContent}>      
+        <PDFViewer />
+      </div>
     </div>
   );
+  
 };
 
 export default Write;
