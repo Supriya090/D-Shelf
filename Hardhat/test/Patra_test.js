b8f7346cf51cdb61dc90e12205906c6779c39792
Rahul Shah <075bct063.rahul@pcampus.edu.np>
2022-02-02 23:24:18 +0545

    getContentIndexByID added , problem in SafeTransferFrom

--- Hardhat/test/Patra_test.js
+++ Hardhat/test/Patra_test.js
@@ -14,6 +14,43 @@ describe("Contract", function () {
     owner = accounts[0];
   });
 
+  it("Mint One Content", async function () {
+    content = {
+      tokenId:[],
+      tokenType : 1,
+      contentType : 1,
+      publicationDate:212112,
+      author:"Rahul Shah",
+      authorAddr: accounts[1].address,
+      ipfsHash: "rahul.com.np",
+      coverImageHash: "Image",
+      onBid : false,
+      descriptionHash : "description",
+      Price : 500,
+      isBurnt :false
+    }
+      await dshelf.connect(owner).mint(content, { value: ethers.utils.parseEther("1.0"),} );
+      let contentId = await dshelf.getTokensOwnedByUser(owner.address);
+      // console.log(contentId);
+      // expect(await dshelf.getTokensOwnedByUser(owner.address)).to.equal([...Array(30).keys()]);
+      // console.log(await dshelf.getContentofToken(0));
+
+      await dshelf.listForSale(contentId[0], 50)
+      expect(await dshelf.isListed(contentId[0])).to.equal(true);
+      expect(await dshelf.getContentPrice(contentId[0])).to.equal(50);
+      await dshelf.removeFromSale(contentId[0]);
+      expect(await dshelf.isListed(contentId[0])).to.equal(false);
+
+
+      await dshelf.listForSale(contentId[0], 10)
+      expect(await dshelf.isListed(contentId[0])).to.equal(true);
+      expect(await dshelf.getContentPrice(contentId[0])).to.equal(10);
+      await dshelf.connect(accounts[15]).buyToken(contentId[0], { value: ethers.utils.parseEther("50.0")});
+      // console.log(await dshelf.getTokensOwnedByUser(accounts[15].address));
+      // expect(await dshelf.getTokensOwnedByUser(accounts[15].address)).to.equal([contentId[0]]);
+      // console.log((await dshelf.getContentofToken(contentId[0]).toNumber()));
+  });
+/*
   it("Mint Batch", async function () {
     content = {
       tokenId:[],
@@ -32,11 +69,11 @@ describe("Contract", function () {
       await dshelf.connect(owner).mintBatch(content, 5, 10, 15, { value: ethers.utils.parseEther("3.0"),} );
       console.log(await dshelf.getTokensOwnedByUser(owner.address));
       // expect(await dshelf.getTokensOwnedByUser(owner.address)).to.equal([...Array(30).keys()]);
-      console.log(await dshelf.getContentofToken(0));
+      console.log(await dshelf.getContentofToken(1));
       for(i = 1; i < 11; i++){
         await dshelf.burnOffensiveContent(i);
       }
-      console.log(await dshelf.getContentofToken(5));
+      console.log(await dshelf.getContentofToken(15));
       // expect((await dshelf.getTokensOwnedByUser(owner.address)).length).to.equal(20);
 
       // console.log(await dshelf.listForSale(11, 50))
@@ -54,4 +91,5 @@ describe("Contract", function () {
       // console.log(await dshelf.getContentofToken(15));
   });
   
+*/
 });
